8af375376de9686e11fe90649962faa395fd6dda
Florian Tischler <florian.tischler@gmx.net>
2019-01-18 10:39:33 +0100

    final demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Flug } from './entity/flug';
+import { FlugService } from './services/flug.service';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  title = 'Hello World!';
+export class AppComponent implements OnInit {
+  public von: string;
+  public nach: string;
+  public fluege: Flug[];
+
+  constructor(private flugService: FlugService) { }
+
+  ngOnInit(): void {
+    this.von = 'Wien';
+    this.nach = 'Berlin';
+
+    this.flugSuchen();
+  }
+
+  public flugSuchen(): void {
+    this.fluege = this.flugService.flugSuchen(this.von, this.nach);
+  }
 }
