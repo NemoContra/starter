91f69c330f33e217eb5e1b628c42f467daf28d5f
Marco Hämmerle <marcohaemmerle@gmx.de>
2019-01-18 15:57:47 +0100

    first task

---
+++ src/app/flight-booking/flight-search/flight.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Flight } from '../../entities/flight';
+import { AbstractFlightService } from './abstract-flight.service';
+
+@Injectable({ providedIn: 'root' })
+export class FlightService implements AbstractFlightService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  find(from: string, to: string): Observable<Flight[]> {
+    let url = 'http://www.angular.at/api/flight';
+
+    let headers = new HttpHeaders()
+      .set('Accept', 'application/json');
+
+    let params = new HttpParams()
+      .set('from', from)
+      .set('to', to);
+
+    return this.http.get<Flight[]>(url, {headers, params});
+  }
+
+  save(flight: Flight): Observable<Flight> {
+    let url = 'http://www.angular.at/api/flight';
+
+    let headers = new HttpHeaders()
+      .set('Accept', 'application/json');
+
+    return this.http.post<Flight>(url, flight, { headers });
+  }
+
+}
