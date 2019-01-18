2a6945bc0837b5bed1f02358e700dd683f45b561
Florian Tischler <florian.tischler@gmx.net>
2019-01-18 10:39:33 +0100

    final demo

---
+++ src/app/flug-suchen/flug-suchen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flug-suchen',
+  templateUrl: './flug-suchen.component.html',
+  styleUrls: ['./flug-suchen.component.css']
+})
+export class FlugSuchenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
