71e778f40dd48b84282809ddfc83cab231c5d809
Florian Tischler <florian.tischler@gmx.net>
2019-01-18 11:16:10 +0100

    final token demo

---
+++ src/app/services/advanced-flug.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { FlugService } from './flug.service';
+import { fluege, Flug } from '../entity/flug';
+
+@Injectable()
+export class AdvancedFlugService implements FlugService {
+  private fluege: Flug[] = fluege;
+
+  flugSuchen(von: string, nach: string): Flug[] {
+    if (von && nach) {
+      return this.fluege.filter(flug => flug.from === von && flug.to === nach);
+    } else {
+      return [];
+    }
+  }
+}
