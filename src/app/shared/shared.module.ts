b35a5b7bb9bf22645076ae267668ce71bd0bed24
Marco Hämmerle <marcohaemmerle@gmx.de>
2019-01-19 18:09:26 +0100

    tasks for forms and form validation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CityPipe } from './city.pipe';
+import { CityValidatorDirective } from './validation/city-validator.directive';
+import { AsyncCityValidatorDirective } from './validation/async-city-validator.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    CityPipe
+    CityPipe,
+    CityValidatorDirective,
+    AsyncCityValidatorDirective
   ],
   exports: [
-    CityPipe
+    CityPipe,
+    CityValidatorDirective,
+    AsyncCityValidatorDirective
   ]
 })
 export class SharedModule {
