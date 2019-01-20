57c4d531552a37369be492f15a1ea7b771668443
Florian Tischler <florian.tischler@gmx.net>
2019-01-20 17:01:41 +0100

    http client final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Flug } from './entity/flug';
 import { FlugService } from './services/flug.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'flight-app',
@@ -10,18 +11,11 @@ import { FlugService } from './services/flug.service';
 export class AppComponent implements OnInit {
   public von: string;
   public nach: string;
-  public fluege: Flug[];
 
   constructor(private flugService: FlugService) { }
 
   ngOnInit(): void {
     this.von = 'Wien';
     this.nach = 'Berlin';
-
-    this.flugSuchen();
-  }
-
-  public flugSuchen(): void {
-    this.fluege = this.flugService.flugSuchen(this.von, this.nach);
   }
 }
--- src/app/services/flug.service.ts
+++ src/app/services/flug.service.ts
@@ -1,15 +1,59 @@
 import { Injectable } from '@angular/core';
-import { fluege, Flug } from '../entity/flug';
+import { Flug } from '../entity/flug';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Element, ElementCompact, xml2js } from 'xml-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlugService {
-  private fluege: Flug[] = fluege;
+  constructor(private httpClient: HttpClient) { }
 
-  public flugSuchen(von: string, nach: string): Flug[] {
-    return this.fluege.filter(flug => {
-      return flug.from === von && flug.to === nach;
+  public getFluege(): Observable<Flug[]> {
+    return this.httpClient.get<Flug[]>('http://www.angular.at/api/flight');
+  }
+
+  public getFlugXML(id: number): Observable<Element | ElementCompact> {
+    const httpParams = new HttpParams().set('id', `${id}`);
+    const httpHeaders = new HttpHeaders()
+      .set('Content-Type', 'application/xml')
+      .set('Accept', 'application/xml');
+
+    return this.httpClient.get('http://www.angular.at/api/flight', {
+      params: httpParams,
+      headers: httpHeaders,
+      responseType: 'text'
+    }).pipe(
+      map(xml => xml2js(xml))
+    );
+  }
+
+  public getFlug(id: number): Observable<Flug> {
+    const httpParams = new HttpParams().set('id', `${id}`);
+
+    return this.httpClient.get<Flug>('http://www.angular.at/api/flight', {
+      params: httpParams
     });
+  }
+
+  public speichern(flug: Flug): Observable<undefined> {
+    return this.httpClient.post<undefined>('http://www.angular.at/api/flight', flug);
+  }
+
+  public anlegen(flug: Flug): Observable<undefined> {
+    return this.httpClient.put<undefined>(`http://www.angular.at/api/flight`, flug);
+  }
+
+  public entfernen(flug: Flug): Observable<undefined> {
+    const httpParams = new HttpParams().set('id', `${flug.id}`);
+    return this.httpClient.delete<undefined>('http://www.angular.at/api/flight', {params: httpParams});
+  }
+
+  public getFlugAbflugDatum(id: number): Observable<Date> {
+    return this.getFlug(id).pipe(
+      map(flug => new Date(flug.date))
+    );
   }
 }
