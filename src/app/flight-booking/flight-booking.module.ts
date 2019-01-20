80ea7cab02c54fb956724ae8e64846ced782c7b9
Marco Hämmerle <marcohaemmerle@gmx.de>
2019-01-20 15:33:41 +0100

    tasks for reactive forms

--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightCardComponent } from './flight-card/flight-card.component';
@@ -14,6 +14,7 @@ import { PassengerSearchComponent } from './passenger-search/passenger-search.co
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(FLIGHT_BOOKING_ROUTES),
     SharedModule
   ],
--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 import { FlightService } from '../flight-search/flight.service';
 import { Flight } from '../../entities/flight';
+import { validateCity } from '../../shared/validation/city-validator';
+import { validateRoundTrip } from '../../shared/validation/round-trip-validator';
 
 @Component({
   selector: 'app-flight-edit',
@@ -17,9 +20,13 @@ export class FlightEditComponent implements OnInit {
   flight: Flight;
   errors: string;
 
+  editForm: FormGroup;
+
   constructor(
     private route: ActivatedRoute,
-    private flightService: FlightService) { }
+    private flightService: FlightService,
+    private fb: FormBuilder) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -28,11 +35,34 @@ export class FlightEditComponent implements OnInit {
         this.showDetails = params['showDetails'];
 
         this.flightService.findById(this.id).subscribe(
-          flight => { this.flight = flight; this.errors = ''; },
-          err => { this.errors = 'Fehler beim Laden'; }
+          flight => {
+            this.flight = flight;
+            this.errors = '';
+          },
+          err => {
+            this.errors = 'Fehler beim Laden';
+          }
         );
       }
     );
+
+    this.editForm = this.fb.group({
+      id:   [1],
+      from: [null, [Validators.required, Validators.minLength(3), validateCity(['Vienna', 'Berlin', 'Gleisdorf'])]],
+      to:   [null],
+      date: [null]
+    });
+
+    this.editForm.validator = validateRoundTrip;
+
+    console.log(this.editForm.value);
+    console.log(this.editForm.valid);
+    console.log(this.editForm.touched);
+    console.log(this.editForm.dirty);
+
+    this.editForm.valueChanges.subscribe(v => {
+      console.debug('changes', v);
+    });
   }
 
   save() {
@@ -41,7 +71,9 @@ export class FlightEditComponent implements OnInit {
         this.flight = flight;
         this.errors = 'Saving was successful!';
       },
-      err => { this.errors = 'Error saving data'; }
+      err => {
+        this.errors = 'Error saving data';
+      }
     );
   }
 
