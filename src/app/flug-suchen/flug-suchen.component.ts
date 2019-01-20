cbd1ef911d3d98c385839b85008e8d09dd8669df
Florian Tischler <florian.tischler@gmx.net>
2019-01-20 17:01:41 +0100

    http client final

--- src/app/flug-suchen/flug-suchen.component.ts
+++ src/app/flug-suchen/flug-suchen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Flug, testFlug } from '../entity/flug';
+import { FlugService } from '../services/flug.service';
 
 @Component({
   selector: 'app-flug-suchen',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flug-suchen.component.css']
 })
 export class FlugSuchenComponent implements OnInit {
+  public fluege: Flug[];
+  public error: string;
 
-  constructor() { }
+  public testFlug: Flug = testFlug;
+
+  constructor(private flugService: FlugService) { }
 
   ngOnInit() {
+    this.getFluege();
   }
 
+  public getFluege(): void {
+    this.flugService.getFluege().subscribe(fluege => {
+      this.fluege = fluege;
+    }, error => {
+      this.error = `Fehler: ${error}`;
+    });
+  }
+
+  public getFlug(id: number): void {
+    this.flugService.getFlug(id).subscribe(console.log);
+  }
+
+  public speichereFlug(): void {
+    this.flugService.speichern(this.testFlug).subscribe(console.log);
+  }
+
+  public entferneFlug(): void {
+    this.flugService.entfernen(this.testFlug).subscribe(console.log);
+  }
 }
