7ac07d2d4595ff4b3c9a8feda35f580ec6032338
Florian Tischler <florian.tischler@gmx.net>
2019-07-12 15:33:41 +0200

    Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,40 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, Subject, Subscription } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Flight } from './flight';
-import { debounceTime, distinctUntilChanged, switchMap, takeUntil } from 'rxjs/operators';
+import { exhaustMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  public flights: Flight[];
+export class AppComponent implements OnInit {
+  // Flights als Observable speichern und mit Async-Pipe übers Template
+  // subscriben
+  public flights$: Observable<Flight[]>;
+
+  // OnClickSubject wird als "MessageBus" verwendet, um den Button-Click
+  // mit Operatoren versehen zu können
+  public onClick$ = new Subject<void>();
 
   public formGroup = new FormGroup({
     von: new FormControl('')
   });
 
-  private subscription = new Subscription();
-
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-    this.subscription.add(this.formGroup.get('von').valueChanges.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      switchMap(von => this.getFlights(von))
-    ).subscribe(flights => this.flights = flights));
-
-    this.subscription.add(this.formGroup.valueChanges.subscribe());
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  onSubmit(): void {
-    //
+    // Hier verwenden wir Operatoren, um vom Click auf den Http-Request
+    // springen zu können:
+    this.flights$ = this.onClick$.pipe(
+      // Wir mappen den leeren Output von onClick$ auf den Wert der FormControl
+      map(() => this.formGroup.get('von').value),
+      // ExhaustMap wartet mit dem nächsten Request immer bis der vorherige
+      // beantwortet wurde, egal wie oft der User klickt
+      exhaustMap(von => this.getFlights(von))
+    );
   }
 
   getFlights(from: string): Observable<Flight[]> {
