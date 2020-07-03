b4cf0c65d3b66bb8b095df9e157322ff12ba575a
Florian Tischler <florian.tischler@gmx.net>
2020-07-03 13:50:49 +0200

    RxJS Demo

--- src/app/flight-booking/flight-search/flight-search.component.ts
+++ src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Flight } from '../../entities/flight';
 import { FlightService } from './flight.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, exhaustMap, filter, mergeMap, switchMap } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-flight-search',
@@ -9,36 +13,41 @@ import { FlightService } from './flight.service';
 export class FlightSearchComponent implements OnInit {
   from: string;
   to: string;
-  flights: Array<Flight> = [];
   selectedFlight: Flight;
   message: string;
+
+  flights$: Observable<Flight[]>;
+
+  formGroup = new FormGroup({
+    from: new FormControl('', [Validators.required]),
+    to: new FormControl('', [Validators.required])
+  });
 
   basket: object = {   // <-- Neue Eigenschaft
     '3': true,
     '5': true
   };
 
-  constructor(private flightService: FlightService) {
+  constructor(private flightService: FlightService,
+              private activedRoute: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit(): void {
-  }
+    // Other operators than switchMap: concatMap, exhaustMap, mergeMap.
+    // switchMap is the right one here. ExhaustMap is better for a button click.
+    // Put Chrome Network tab to high latency and vary your typing speed to test this
 
-  search(): void {
-    if (!this.from || !this.to) {
-      return;
-    }
+    this.flights$ = this.formGroup.valueChanges.pipe(
+      filter(() => this.formGroup.valid),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(({from, to}) => this.flightService.find(from, to))
+    );
 
-    this.flightService
-      .find(this.from, this.to)
-      .subscribe(
-        (flights) => {
-          this.flights = flights;
-        },
-        (errResp) => {
-          console.error('Error loading flights', errResp);
-        }
-      );
+    /*this.router.navigate(['1234'], {
+      relativeTo: this.activedRoute,
+      skipLocationChange: false
+    });*/
   }
 
   select(f: Flight): void {
