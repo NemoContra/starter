ded87fb47ef8afc2b7e93bc2d98c82c875d916a5
Florian Tischler <florian.tischler@gmx.net>
2020-07-03 12:17:34 +0200

    Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'Hello World!';
 }
